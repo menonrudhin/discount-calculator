ba6603a6b8bb6773ca22edfb8190cfb4b3aa351d
Rudhin <menonrudhin@gmail.com>
2020-07-18 17:25:16 -0400

    Initial commit
    Screenshot feature works
    Need to work on discount calculation and button style

---
+++ app/src/main/java/com/discountcalculator/FirstFragment.java
@@ -0,0 +1,35 @@
+package com.discountcalculator;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btnCalculate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getContext(), "Clicked", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+}
